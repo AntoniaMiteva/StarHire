90f638673f00bb13b3f3a9e97fe96f07a5e0f3d3
Antonia <kris.tina.ivn.07@gmail.com>
2026-02-08 16:55:05 +0200

    git commit -m "Add Jobs ViewModels for list, details and create"

---
+++ StarHire/StarHire/ViewModels/Jobs/JobListVM.cs
@@ -0,0 +1,12 @@
+﻿namespace StarHire.ViewModels.Jobs
+{
+    public class JobListVM
+    {
+        
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Planet { get; set; }
+            public decimal Salary { get; set; }
+        
+    }
+}
