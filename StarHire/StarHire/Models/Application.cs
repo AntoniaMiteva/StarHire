5ca277c79b1a38fdb968d9a2e8fd2498e94e2afd
Antonia <kris.tina.ivn.07@gmail.com>
2026-02-08 14:38:23 +0200

    git add StarHire/Data/ApplicationDbContext.cs

---
+++ StarHire/StarHire/Models/Application.cs
@@ -0,0 +1,15 @@
+﻿namespace StarHire.Models
+{
+    public class Application
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public int JobId { get; set; }
+
+        public Job Job { get; set; }
+
+        public string Status { get; set; } = "Pending";
+    }
+}
