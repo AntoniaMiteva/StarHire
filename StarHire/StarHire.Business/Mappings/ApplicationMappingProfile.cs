fe26c0984962806159a86b4c35780650f90a87b3
Kristina <kris.tina.ivn.07@gmail.com>
2026-02-14 11:37:42 +0200

    add automapper

---
+++ StarHire/StarHire.Business/Mappings/ApplicationMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using StarHire.Models.Domain.Entities;
+using StarHire.Models.ViewModels.Applications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarHire.Business.Mappings
+{
+    public class ApplicationMappingProfile : Profile
+    {
+        public ApplicationMappingProfile()
+        {
+            CreateMap<Application, ApplicationViewModel>();
+            //CreateMap <ApplicationCreateOrEditViewModel, Application>();
+        }
+    }
+}
