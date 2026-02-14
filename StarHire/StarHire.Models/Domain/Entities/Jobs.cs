c878fee0b7260b8bd39a22b10599d36f6f0a39a0
Kristina <kris.tina.ivn.07@gmail.com>
2026-02-14 11:36:46 +0200

    add entities

---
+++ StarHire/StarHire.Models/Domain/Entities/Jobs.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace StarHire.Models.Domain.Entities
+{
+    public class Job
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }  = string.Empty;
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        public decimal Salary { get; set; }
+
+        public string Planet { get; set; } = string.Empty;
+
+        public Guid EmployeerId { get; set; }
+        public IdentityUser Employeer { get; set; }
+
+        public ICollection<Application> Applications { get; set; } = new List<Application>();
+    }
+
+}
---
+++ StarHire/StarHire.Models/Domain/Enums/ApplicationStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace StarHire.Models
+{
+    public enum ApplicationStatus
+    {
+        Pending,
+        Approved,
+        Rejected
+    }
+}
