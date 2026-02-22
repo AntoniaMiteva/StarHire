04f43bce6739e1b705a915684f54f560834ece3e
AntoniaMiteva <antoniakmiteva@gmail.com>
2026-02-22 09:59:53 +0200

    Fix: add message field to application and mapping

---
+++ StarHire/StarHire.Data/Migrations/20260222075504_AddMessageToApplication.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StarHire.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMessageToApplication : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Message",
+                table: "Applications",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Message",
+                table: "Applications");
+        }
+    }
+}
