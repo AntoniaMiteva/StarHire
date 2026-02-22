6be68e3e167e19e48191364e081cd449cf5e0c80
Karina Poptraykova <karinapoptraykova@gmail.com>
2026-02-22 09:34:59 +0200

    Register: Drop-down menu for roles (Alien, Employeer)

--- StarHire/StarHire/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ StarHire/StarHire/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -98,6 +98,9 @@ namespace StarHire.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+            [Required]
+            [Display(Name = "Role")]
+            public string Role { get; set; }
         }
 
 
@@ -122,7 +125,7 @@ namespace StarHire.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
-
+                    await _userManager.AddToRoleAsync(user, Input.Role);
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
