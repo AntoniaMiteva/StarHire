6985ae22dcafdf88213227b8102a702bf8acd34a
AntoniaMiteva <134741961+AntoniaMiteva@users.noreply.github.com>
2026-04-05 00:16:39 +0300

    Add GetRecommendedAsync to IJobService

--- StarHire/StarHire.Business/Services/Interfaces/IJobService.cs
+++ StarHire/StarHire.Business/Services/Interfaces/IJobService.cs
@@ -1,4 +1,4 @@
-﻿using StarHire.Models.ViewModels.Applications;
+using StarHire.Models.ViewModels.Applications;
 using StarHire.Models.ViewModels.Jobs;
 using StarHire.Models.ViewModels.Skills;
 using System;
@@ -7,17 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace StarHire.Business.Services.Interfaces
 {
-    public  interface IJobService
+    public interface IJobService
     {
-        //Task<List<JobViewModel>> GetAll(string? search, string? planet, decimal? minSalary);
         Task<JobViewModel?> GetById(Guid id);
         Task Create(CreateJobViewModel model, Guid employerId);
         Task<List<JobViewModel>> GetByEmployer(Guid employerId);
         Task<List<ApplicantViewModel>> GetApplicants(Guid jobId, Guid employerId);
         Task<List<SkillCheckboxViewModel>> GetAllSkillsAsync();
         Task<List<JobViewModel>> GetAll(string? search, string? planet, decimal? minSalary, Guid? userId = null);
+        Task<IEnumerable<JobViewModel>> GetRecommendedAsync(Guid userId);
     }
 }
